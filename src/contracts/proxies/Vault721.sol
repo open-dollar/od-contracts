848eea935334ce14f01e8f888b5cb047e1b5bfd2
Hunter King <hking6776@gmail.com>
2023-07-26 15:12:55 -0500

    add comments

--- src/contracts/proxies/Vault721.sol
+++ src/contracts/proxies/Vault721.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
 
+import {ERC721} from '@openzeppelin/tokens/ERC721.sol';
+
+contract Vault721 is ERC721 {}
+
+// Vars to inlcude in NFT
+
+// Collateral Type
+//   contract: HaiSafeManager
+//   function: safeData(uint256 _safe)
+
+// Collateral Ratio
+//   contract: OracleRelayer
+//   function: cParams(bytes32 _cType)
+
+// Stability Fee
+//   contract: TaxCollector
+//   function: cParams(bytes32 _cType)
+
+// Liquidation Penalty
+//   contract: LiquidationEngine
+//   function: cParams(bytes32 _cType)
