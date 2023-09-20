17fce894379e18f67272d4f764b40e706e671fcf
Patrick Gallagher <blockchainbuddha@gmail.com>
2023-09-20 16:59:42 -0700

    Finalize script

--- tasks/parseDeployments.js
+++ tasks/parseDeployments.js
@@ -49,7 +49,6 @@ fs.readFile(filePath, "utf8", (err, data) => {
   console.log("Deployment file parsed successfully!");
 
   createGoerliDeploymentsFile(contracts);
-  createOutputFile(contracts);
 });
 
 const createGoerliDeploymentsFile = (contracts) => {
@@ -72,115 +71,6 @@ ${addressText}
       return;
     }
 
-    console.log("JSON object written to file successfully!");
-  });
-};
-
-const createOutputFile = (contracts) => {
-  // VERIFY!!!
-  const ETH_ADDRESS = "0xEe01c0CD76354C383B8c7B4e65EA88D00B06f36f";
-
-  const final = {
-    MULTICALL: "0xcA11bde05977b3631167028862bE2a173976CA11",
-    ETH: ETH_ADDRESS,
-    GEB_SYSTEM_COIN: contracts.SystemCoin,
-    GEB_PROTOCOL_TOKEN: contracts.ProtocolToken,
-    GEB_SAFE_ENGINE: contracts.SAFEEngine,
-    GEB_ORACLE_RELAYER: contracts.OracleRelayer,
-    GEB_SURPLUS_AUCTION_HOUSE: contracts.SurplusAuctionHouse,
-    GEB_DEBT_AUCTION_HOUSE: contracts.DebtAuctionHouse,
-    GEB_COLLATERAL_AUCTION_HOUSE_FACTORY:
-      contracts.CollateralAuctionHouseFactory,
-    GEB_ACCOUNTING_ENGINE: contracts.AccountingEngine,
-    GEB_LIQUIDATION_ENGINE: contracts.LiquidationEngine,
-    GEB_COIN_JOIN: contracts.CoinJoin,
-    GEB_COLLATERAL_JOIN_FACTORY: contracts.CollateralJoinFactory,
-    GEB_TAX_COLLECTOR: contracts.TaxCollector,
-    GEB_STABILITY_FEE_TREASURY: contracts.StabilityFeeTreasury,
-    GEB_GLOBAL_SETTLEMENT: contracts.GlobalSettlement,
-    GEB_POST_SETTLEMENT_SURPLUS_AUCTION_HOUSE:
-      contracts.PostSettlementSurplusAuctionHouse,
-    GEB_POST_SETTLEMENT_SURPLUS_AUCTIONEER:
-      contracts.SettlementSurplusAuctioneer,
-    GEB_RRFM_SETTER: contracts.PIDRateSetter,
-    GEB_RRFM_CALCULATOR: contracts.PIDController,
-    SAFE_MANAGER: contracts.ODSafeManager,
-    GEB_GLOBAL_SETTLEMENT: contracts.GlobalSettlement,
-    PROXY_FACTORY: contracts.Vault721,
-    PROXY_BASIC_ACTIONS: contracts.BasicActions,
-    PROXY_REGISTRY: contracts.Vault721,
-    PROXY_DEBT_AUCTION_ACTIONS: contracts.DebtBidActions,
-    PROXY_SURPLUS_AUCTION_ACTIONS: contracts.SurplusBidActions,
-    PROXY_COLLATERAL_AUCTION_ACTIONS: contracts.CollateralBidActions,
-    PROXY_POST_SETTLEMENT_SURPLUS_AUCTION_ACTIONS:
-      contracts.PostSettlementSurplusBidActions,
-    PROXY_GLOBAL_SETTLEMENT_ACTIONS: contracts.GlobalSettlementActions,
-    PROXY_REWARDED_ACTIONS: contracts.RewardedActions,
-    JOB_ACCOUNTING: contracts.AccountingJob,
-    JOB_LIQUIDATION: contracts.LiquidationJob,
-    JOB_ORACLES: contracts.OracleJob,
-  };
-
-  const collateral = {
-    OD: {
-      address: contracts.SystemCoin,
-    },
-    ODG: {
-      address: contracts.ProtocolToken,
-    },
-    WETH: {
-      address: ETH_ADDRESS,
-      collateralJoin: contracts.CollateralJoinChild_WETH,
-      collateralAuctionHouse: contracts.CollateralAuctionHouseChild_WETH,
-    },
-    WBTC: {
-      address: contracts.Erc20ForTestnetWBTC,
-      collateralJoin: contracts.CollateralJoinChild_WBTC,
-      collateralAuctionHouse: contracts.CollateralAuctionHouseChild_WBTC,
-    },
-    FTRG: {
-      address: contracts.Erc20ForTestnetFTRG,
-      collateralJoin: contracts.CollateralJoinDelegatableChild_FTRG,
-      collateralAuctionHouse: contracts.CollateralAuctionHouseChild_FTRG,
-    },
-    STN: {
-      address: contracts.Erc20ForTestnetSTONES,
-      collateralJoin: contracts.CollateralJoinChild_STONES,
-      collateralAuctionHouse: contracts.CollateralAuctionHouseChild_STONES,
-    },
-    TOTEM: {
-      address: contracts.Erc20ForTestnetTOTEM,
-      collateralJoin: contracts.CollateralJoinChild_TOTEM,
-      collateralAuctionHouse: contracts.CollateralAuctionHouseChild_TOTEM,
-    },
-  };
-  const validate = (obj) => {
-    const missing = Object.values(obj).reduce((acc, curr, i) => {
-      if (!curr) {
-        acc.push(Object.keys(obj)[i]);
-      }
-      return acc;
-    }, []);
-    if (missing.length) throw `Missing values: ${missing.join(", ")}`;
-  };
-
-  validate(final);
-
-  const outputPath = path.join(__dirname, "./output.js");
-  const content = `// WARNING: You must verify the ETH address is still correct 
-// which is used in both 'addresses' and 'collateral'
-
-
-const addresses = ${JSON.stringify(final, null, 2)}
-  
-const collateral = ${JSON.stringify(collateral, null, 2)}`;
-
-  fs.writeFile(outputPath, content, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    console.log("JSON object written to file successfully!");
+    console.log("GoerliContracts.s.sol written to file successfully!");
   });
 };
--- tasks/prepareForSdk.js
+++ tasks/prepareForSdk.js
@@ -9,98 +9,110 @@ fs.readFile(filePath, "utf8", (err, data) => {
     return;
   }
 
-  const cleanedString = data
-    .replace(/address public /g, "")
-    .replace(/;/g, "")
-    .replace(/ /g, "");
-
+  const onlyAddresses = data.split("Contracts {")[1].split("}")[0];
+  const cleanedString = onlyAddresses
+    .replace(/address public/g, "")
+    .replace(/\n/g, "")
+    .replace(/\s/g, "");
   // Split the string into lines
-  const lines = cleanedString.split("\n");
+  const lines = cleanedString.split(";");
 
   // Create an empty object to store the converted values
-  const parsed = {};
-
+  const contracts = {};
   // Iterate over each line and populate the output object
   lines.forEach((line) => {
     const [key, value] = line.split("=");
-    parsed[key.trim()] = value?.trim();
+    contracts[key.trim()] = value?.trim();
   });
 
+  createOutputFile(contracts);
+});
+
+const createOutputFile = (contracts) => {
   // VERIFY!!!
   const ETH_ADDRESS = "0xEe01c0CD76354C383B8c7B4e65EA88D00B06f36f";
-
   const final = {
     MULTICALL: "0xcA11bde05977b3631167028862bE2a173976CA11",
     ETH: ETH_ADDRESS,
-    GEB_SYSTEM_COIN: parsed.systemCoinAddr,
-    GEB_PROTOCOL_TOKEN: parsed.protocolTokenAddr,
-    GEB_SAFE_ENGINE: parsed.safeEngineAddr,
-    GEB_ORACLE_RELAYER: parsed.oracleRelayerAddr,
-    GEB_SURPLUS_AUCTION_HOUSE: parsed.surplusAuctionHouseAddr,
-    GEB_DEBT_AUCTION_HOUSE: parsed.debtAuctionHouseAddr,
+    GEB_SYSTEM_COIN: contracts.SystemCoin_Address,
+    GEB_PROTOCOL_TOKEN: contracts.ProtocolToken_Address,
+    GEB_SAFE_ENGINE: contracts.SAFEEngine_Address,
+    GEB_ORACLE_RELAYER: contracts.OracleRelayer_Address,
+    GEB_SURPLUS_AUCTION_HOUSE: contracts.SurplusAuctionHouse_Address,
+    GEB_DEBT_AUCTION_HOUSE: contracts.DebtAuctionHouse_Address,
     GEB_COLLATERAL_AUCTION_HOUSE_FACTORY:
-      parsed.collateralAuctionHouseFactoryAddr,
-    GEB_ACCOUNTING_ENGINE: parsed.accountingEngineAddr,
-    GEB_LIQUIDATION_ENGINE: parsed.liquidationEngineAddr,
-    GEB_COIN_JOIN: parsed.coinJoinAddr,
-    GEB_COLLATERAL_JOIN_FACTORY: parsed.collateralJoinFactoryAddr,
-    GEB_TAX_COLLECTOR: parsed.taxCollectorAddr,
-    GEB_STABILITY_FEE_TREASURY: parsed.stabilityFeeTreasuryAddr,
-    GEB_GLOBAL_SETTLEMENT: parsed.globalSettlementAddr,
+      contracts.CollateralAuctionHouseFactory_Address,
+    GEB_ACCOUNTING_ENGINE: contracts.AccountingEngine_Address,
+    GEB_LIQUIDATION_ENGINE: contracts.LiquidationEngine_Address,
+    GEB_COIN_JOIN: contracts.CoinJoin_Address,
+    GEB_COLLATERAL_JOIN_FACTORY: contracts.CollateralJoinFactory_Address,
+    GEB_TAX_COLLECTOR: contracts.TaxCollector_Address,
+    GEB_STABILITY_FEE_TREASURY: contracts.StabilityFeeTreasury_Address,
+    GEB_GLOBAL_SETTLEMENT: contracts.GlobalSettlement_Address,
     GEB_POST_SETTLEMENT_SURPLUS_AUCTION_HOUSE:
-      parsed.postSettlementSurplusAuctionHouseAddr,
+      contracts.PostSettlementSurplusAuctionHouse_Address,
     GEB_POST_SETTLEMENT_SURPLUS_AUCTIONEER:
-      parsed.settlementSurplusAuctioneerAddr,
-    GEB_RRFM_SETTER: parsed.PIDRateSetterAddr,
-    GEB_RRFM_CALCULATOR: parsed.PIDControllerAddr,
-    SAFE_MANAGER: parsed.odSafeManagerAddr,
-    GEB_GLOBAL_SETTLEMENT: parsed.globalSettlementAddr,
-    PROXY_FACTORY: parsed.vault721Addr,
-    PROXY_BASIC_ACTIONS: parsed.basicActionsAddr,
-    PROXY_REGISTRY: parsed.vault721Addr,
-    PROXY_DEBT_AUCTION_ACTIONS: parsed.debtBidActionsAddr,
-    PROXY_SURPLUS_AUCTION_ACTIONS: parsed.surplusBidActionsAddr,
-    PROXY_COLLATERAL_AUCTION_ACTIONS: parsed.collateralBidActionsAddr,
+      contracts.SettlementSurplusAuctioneer_Address,
+    GEB_RRFM_SETTER: contracts.PIDRateSetter_Address,
+    GEB_RRFM_CALCULATOR: contracts.PIDController_Address,
+    SAFE_MANAGER: contracts.ODSafeManager_Address,
+    GEB_GLOBAL_SETTLEMENT: contracts.GlobalSettlement_Address,
+    PROXY_FACTORY: contracts.Vault721_Address,
+    PROXY_BASIC_ACTIONS: contracts.BasicActions_Address,
+    PROXY_REGISTRY: contracts.Vault721_Address,
+    PROXY_DEBT_AUCTION_ACTIONS: contracts.DebtBidActions_Address,
+    PROXY_SURPLUS_AUCTION_ACTIONS: contracts.SurplusBidActions_Address,
+    PROXY_COLLATERAL_AUCTION_ACTIONS: contracts.CollateralBidActions_Address,
     PROXY_POST_SETTLEMENT_SURPLUS_AUCTION_ACTIONS:
-      parsed.postSettlementSurplusBidActionsAddr,
-    PROXY_GLOBAL_SETTLEMENT_ACTIONS: parsed.globalSettlementActionsAddr,
-    PROXY_REWARDED_ACTIONS: parsed.rewardedActionsAddr,
-    JOB_ACCOUNTING: parsed.accountingJobAddr,
-    JOB_LIQUIDATION: parsed.liquidationJobAddr,
-    JOB_ORACLES: parsed.oracleJobAddr,
+      contracts.PostSettlementSurplusBidActions_Address,
+    PROXY_GLOBAL_SETTLEMENT_ACTIONS: contracts.GlobalSettlementActions_Address,
+    PROXY_REWARDED_ACTIONS: contracts.RewardedActions_Address,
+    JOB_ACCOUNTING: contracts.AccountingJob_Address,
+    JOB_LIQUIDATION: contracts.LiquidationJob_Address,
+    JOB_ORACLES: contracts.OracleJob_Address,
   };
 
   const collateral = {
     OD: {
-      address: parsed.systemCoinAddr,
+      address: contracts.SystemCoin_Address,
     },
     ODG: {
-      address: parsed.protocolTokenAddr,
+      address: contracts.ProtocolToken_Address,
     },
     WETH: {
       address: ETH_ADDRESS,
-      collateralJoin: parsed.collateralJoinChild_WETHAddr,
-      collateralAuctionHouse: parsed.collateralAuctionHouseChild_WETHAddr,
-    },
-    WBTC: {
-      address: parsed.erc20ForTestnetWBTCAddr,
-      collateralJoin: parsed.collateralJoinChild_WBTCAddr,
-      collateralAuctionHouse: parsed.collateralAuctionHouseChild_WBTCAddr,
+      collateralJoin:
+        contracts.CollateralJoinChild_0x5745544800000000000000000000000000000000000000000000000000000000_Address,
+      collateralAuctionHouse:
+        contracts.CollateralAuctionHouseChild_0x5745544800000000000000000000000000000000000000000000000000000000_Address,
     },
     FTRG: {
-      address: parsed.erc20ForTestnetFTRGAddr,
-      collateralJoin: parsed.collateralJoinDelegatableChild_FTRGAddr,
-      collateralAuctionHouse: parsed.collateralAuctionHouseChild_FTRGAddr,
+      address: "0x36E91d510D492b87813Bb49704423b3b5885201F",
+      collateralJoin:
+        contracts.CollateralJoinChild_0x4654524700000000000000000000000000000000000000000000000000000000_Address,
+      collateralAuctionHouse:
+        contracts.CollateralAuctionHouseChild_0x4654524700000000000000000000000000000000000000000000000000000000_Address,
+    },
+    WBTC: {
+      address: contracts.MintableERC20_WBTC_Address,
+      collateralJoin:
+        contracts.CollateralJoinChild_0x5742544300000000000000000000000000000000000000000000000000000000_Address,
+      collateralAuctionHouse:
+        contracts.CollateralAuctionHouseChild_0x5742544300000000000000000000000000000000000000000000000000000000_Address,
     },
     STN: {
-      address: parsed.erc20ForTestnetSTONESAddr,
-      collateralJoin: parsed.collateralJoinChild_STONESAddr,
-      collateralAuctionHouse: parsed.collateralAuctionHouseChild_STONESAddr,
+      address: contracts.MintableERC20_STONES_Address,
+      collateralJoin:
+        contracts.CollateralJoinChild_0x53544f4e45530000000000000000000000000000000000000000000000000000_Address,
+      collateralAuctionHouse:
+        contracts.CollateralAuctionHouseChild_0x53544f4e45530000000000000000000000000000000000000000000000000000_Address,
     },
     TOTEM: {
-      address: parsed.erc20ForTestnetTOTEMAddr,
-      collateralJoin: parsed.collateralJoinChild_TOTEMAddr,
-      collateralAuctionHouse: parsed.collateralAuctionHouseChild_TOTEMAddr,
+      address: contracts.MintableERC20_TOTEM_Address,
+      collateralJoin:
+        contracts.CollateralJoinChild_0x544f54454d000000000000000000000000000000000000000000000000000000_Address,
+      collateralAuctionHouse:
+        contracts.CollateralAuctionHouseChild_0x544f54454d000000000000000000000000000000000000000000000000000000_Address,
     },
   };
   const validate = (obj) => {
@@ -123,12 +135,13 @@ fs.readFile(filePath, "utf8", (err, data) => {
 const addresses = ${JSON.stringify(final, null, 2)}
   
 const collateral = ${JSON.stringify(collateral, null, 2)}`;
+
   fs.writeFile(outputPath, content, (err) => {
     if (err) {
       console.error(err);
       return;
     }
 
-    console.log("JSON object written to file successfully!");
+    console.log("output.js written to file successfully!");
   });
-});
+};
