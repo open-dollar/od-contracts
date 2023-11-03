464357c69de9d6af19ab82b20c7ac551e34e7f77
Hunter King <hking6776@gmail.com>
2023-11-03 11:11:18 -0400

    add --skip-simulation script to bypass gas estimations

--- script/user/OpenSafe.s.sol
+++ script/user/OpenSafe.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {TestScripts} from '@script/user/utils/TestScripts.s.sol';
 
 // BROADCAST
-// source .env && forge script OpenSafe --with-gas-price 2000000000 -vvvvv --chain-id 461614 --rpc-url $ARB_SEPOLIA_RPC --broadcast --verifier etherscan --verifier-url $SEPOLIA_API --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
+// source .env && forge script OpenSafe --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC --broadcast
 
 // SIMULATE
 // source .env && forge script OpenSafe --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC
@@ -17,5 +17,3 @@ contract OpenSafe is TestScripts {
     vm.stopBroadcast();
   }
 }
-
-// source .env && forge script DeployGoerli --with-gas-price 2000000000 -vvvvv --chain-id 461614 --rpc-url $ARB_SEPOLIA_RPC --private-key $ARB_SEPOLIA_DEPLOYER_PK --broadcast --verifier etherscan --verifier-url $SEPOLIA_API --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
