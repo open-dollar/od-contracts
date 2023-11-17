ab187ff6f4b4c8cf414894f4f0aef9345202044b
Hunter King <hking6776@gmail.com>
2023-11-17 17:54:10 -0500

    edit svg

--- src/contracts/proxies/NFTRenderer.sol
+++ src/contracts/proxies/NFTRenderer.sol
@@ -42,12 +42,12 @@ contract NFTRenderer {
   }
 
   struct VaultParams {
+    uint256 ratio;
     string collateral;
     string debt;
     string metaCollateral;
     string metaDebt;
     string vaultId;
-    string ratio;
     string stabilityFee;
     string symbol;
     string risk;
@@ -80,6 +80,7 @@ contract NFTRenderer {
   function render(uint256 _safeId) external view returns (string memory uri) {
     VaultParams memory params = renderParams(_safeId);
     string memory text = _renderText(params);
+    uint256 ratio = params.ratio;
 
     string memory json = string.concat(
       '{"name":"OD NFV #',
@@ -89,8 +90,10 @@ contract NFTRenderer {
         bytes(
           string.concat(
             _renderVaultInfo(params.vaultId, params.color),
-            _renderCollatAndDebt(params.stabilityFee, params.debt, params.collateral, params.symbol, params.lastUpdate),
-            _renderRisk(params.stroke, params.risk, params.ratio),
+            _renderCollatAndDebt(
+              ratio, params.stabilityFee, params.debt, params.collateral, params.symbol, params.lastUpdate
+            ),
+            _renderRisk(ratio, params.stroke, params.risk),
             _renderBackground(params.color)
           )
         )
@@ -149,7 +152,7 @@ contract NFTRenderer {
       params.lastUpdate = _formatDateTime(oracle.lastUpdateTime());
       (params.risk, params.color) = _calcRisk(ratio, liquidationCRatio, safetyCRatio);
       params.stroke = _calcStroke(ratio);
-      params.ratio = ratio.toString();
+      params.ratio = ratio;
     }
 
     ITaxCollector.TaxCollectorCollateralData memory taxData = _taxCollector.cData(cType);
@@ -203,7 +206,7 @@ contract NFTRenderer {
       '"},{"trait_type":"Risk","value":"',
       params.risk,
       '"},{"trait_type":"Collateral Ratio","value":"',
-      params.ratio,
+      params.ratio.toString(),
       '"},{"trait_type":"Last Updated","value":"'
     );
   }
@@ -229,22 +232,33 @@ contract NFTRenderer {
    * @dev svg collateral and debt data
    */
   function _renderCollatAndDebt(
+    uint256 ratio,
     string memory stabilityFee,
     string memory debt,
     string memory collateral,
     string memory symbol,
     string memory lastUpdate
   ) internal pure returns (string memory svg) {
+    string memory debtDetail;
+    if (ratio != 0) {
+      debtDetail = string.concat(
+        '<text fill="#00587E" xml:space="preserve" font-weight="600"><tspan x="102" y="168.9">DEBT MINTED</tspan></text><text fill="#D0F1FF" xml:space="preserve" font-size="24"><tspan x="102" y="194">',
+        debt,
+        ' OD',
+        '</tspan></text><text fill="#00587E" xml:space="preserve" font-weight="600"><tspan x="102" y="229.9">COLLATERAL DEPOSITED</tspan></text><text fill="#D0F1FF" xml:space="preserve" font-size="24"><tspan x="102" y="255">',
+        collateral,
+        ' ',
+        symbol,
+        '</tspan></text><text opacity=".3" transform="rotate(-90 326.5 -58.5)" fill="#fff" xml:space="preserve" font-size="10"><tspan x="-10.3" y="7.3">Updated '
+      );
+    } else {
+      debtDetail =
+        '<text fill="#63676F" xml:space="preserve" font-size="24"><tspan x="136" y="210">Zero Balance</tspan></text><text opacity=".3" transform="rotate(-90 326.5 -58.5)" fill="#fff" xml:space="preserve" font-size="10"><tspan x="-10.3" y="7.3">Updated ';
+    }
     svg = string.concat(
       stabilityFee,
-      '%</tspan></text><text opacity=".3" transform="rotate(90 -66.5 101.5)" fill="#fff" xml:space="preserve" font-size="10"><tspan x=".5" y="7.3">opendollar.com</tspan></text><text fill="#00587E" xml:space="preserve" font-weight="600"><tspan x="102" y="168.9">DEBT MINTED</tspan></text><text fill="#D0F1FF" xml:space="preserve" font-size="24"><tspan x="102" y="194">',
-      debt,
-      ' OD',
-      '</tspan></text><text fill="#00587E" xml:space="preserve" font-weight="600"><tspan x="102" y="229.9">COLLATERAL DEPOSITED</tspan></text><text fill="#D0F1FF" xml:space="preserve" font-size="24"><tspan x="102" y="255">',
-      collateral,
-      ' ',
-      symbol,
-      '</tspan></text><text opacity=".3" transform="rotate(-90 326.5 -58.5)" fill="#fff" xml:space="preserve" font-size="10"><tspan x="-10.3" y="7.3">Updated ',
+      '%</tspan></text><text opacity=".3" transform="rotate(90 -66.5 101.5)" fill="#fff" xml:space="preserve" font-size="10"><tspan x=".5" y="7.3">opendollar.com</tspan></text>',
+      debtDetail,
       lastUpdate
     );
   }
@@ -253,17 +267,31 @@ contract NFTRenderer {
    * @dev svg risk data
    */
   function _renderRisk(
+    uint256 ratio,
     string memory stroke,
-    string memory risk,
-    string memory ratio
+    string memory risk
   ) internal pure returns (string memory svg) {
+    string memory rectangle;
+    string memory riskDetail;
+    if (ratio != 0) {
+      rectangle =
+        '), 1005" d="M210 40a160 160 0 0 1 0 320 160 160 0 0 1 0-320" /></g><g class="risk-ratio"><rect x="242" y="306" width="154" height="82" rx="8" fill="#001828" fill-opacity=".7" /><circle cx="243" cy="326.5" r="4" /><text xml:space="preserve" font-weight="600"><tspan x="255" y="330.7">';
+      riskDetail = string.concat(
+        ' RISK</tspan></text><text xml:space="preserve"><tspan x="255" y="355.7">COLLATERAL</tspan><tspan x="255" y="371.7">RATIO ',
+        ratio.toString(),
+        '%</tspan></text>'
+      );
+    } else {
+      rectangle =
+        '), 1005" d="M210 40a160 160 0 0 1 0 320 160 160 0 0 1 0-320" /></g><g class="risk-ratio"><rect x="298" y="350" width="96" height="40" rx="8" fill="#001828" fill-opacity=".7" /><circle cx="243" cy="326.5" r="4" /><text xml:space="preserve" font-weight="600"><tspan x="255" y="330.7">';
+      riskDetail = ' RISK</tspan></text>';
+    }
     svg = string.concat(
       '</tspan></text><g opacity=".6"><text fill="#fff" xml:space="preserve"><tspan x="24" y="387.4">Powered by</tspan></text><path d="M112.5 388c-2 0-3-1.2-3-3.2v-3.3c0-2 1-3.3 3-3.3 2.1 0 3.2 1.3 3.2 3.3v3.3c0 2-1 3.3-3.2 3.3Zm-1.5-3.2c0 1.1.5 1.8 1.6 1.8 1 0 1.5-.7 1.5-1.8v-3.3c0-1.1-.4-1.8-1.5-1.8s-1.6.7-1.6 1.8v3.3ZM117.3 390.6l-.1-.2V381l.1-.2h1.2l.1.2v.7c.3-.7 1-1 1.8-1 1.3 0 2 1 2 2.6v2.3c0 1.6-.8 2.6-2 2.6-.8 0-1.4-.4-1.7-1v3.3c0 .1 0 .2-.2.2h-1.2Zm1.4-5.2c0 .9.5 1.3 1.1 1.3.7 0 1-.5 1-1.3v-2.2c0-.7-.3-1.3-1-1.3-.6 0-1.1.5-1.1 1.4v2ZM126.2 388c-1.6 0-2.6-1-2.6-2.6v-2.2c0-1.6 1-2.6 2.5-2.6 1.6 0 2.6 1 2.6 2.6v1.4c0 .1 0 .2-.2.2h-3.4v.6c0 1 .4 1.4 1.1 1.4.6 0 1-.3 1.1-.8l.2-.2 1 .3c.1 0 .2 0 .1.2-.2 1-1 1.8-2.4 1.8Zm-1.1-4.2h2.2v-.7c0-.8-.4-1.3-1.1-1.3-.8 0-1.1.5-1.1 1.3v.7ZM130.2 388l-.2-.2v-7l.2-.1h1.1c.1 0 .2 0 .2.2v.7c.4-.7 1-1 1.7-1 1.1 0 1.8.8 1.8 2.5v4.7l-.1.1h-1.2l-.2-.1V383c0-.8-.3-1.2-.8-1.2s-1 .4-1.2 1v4.9l-.1.1h-1.2ZM136.8 388l-.2-.2v-9.3c0-.1 0-.2.2-.2h2.6c2 0 3.1 1.3 3.1 3.3v3c0 2-1 3.3-3.1 3.3h-2.6Zm1.4-1.5h1.2c1 0 1.6-.7 1.6-1.8v-3c0-1.2-.5-1.9-1.6-1.9h-1.2v6.7ZM146.4 388c-1.6 0-2.6-1-2.6-2.6v-2.2c0-1.6 1-2.6 2.6-2.6 1.7 0 2.6 1 2.6 2.6v2.2c0 1.7-1 2.7-2.6 2.7Zm-1-2.6c0 .9.3 1.3 1 1.3.8 0 1.1-.4 1.1-1.3v-2.2c0-.8-.4-1.3-1-1.3-.8 0-1.1.5-1.1 1.3v2.2ZM150.6 388l-.2-.2V378l.2-.2h1.2l.2.2v9.7l-.2.1h-1.2ZM153.7 388l-.2-.2V378c0-.1 0-.2.2-.2h1.2l.1.2v9.7l-.1.1h-1.2ZM160 388l-.1-.2v-.8c-.4.7-1 1-1.7 1-1.1 0-1.9-.7-1.9-2 0-1.4.7-2.3 2.6-2.3h1v-.8c0-.7-.4-1-1-1s-.8.2-1 .8h-.2l-1-.2c-.1 0-.2-.1-.1-.2.2-1 1-1.7 2.4-1.7 1.5 0 2.3.7 2.3 2.2v5l-.2.1h-1Zm-2.3-2.2c0 .7.3 1 1 1 .5 0 1-.3 1.1-1v-1.1h-.8c-.8 0-1.3.4-1.3 1.1ZM163 388l-.2-.2v-7h1.5v1c.3-.7.9-1.2 1.8-1.2.1 0 .2 0 .2.2v1.1c0 .1 0 .2-.2.2-1 0-1.5.3-1.8 1v4.7l-.1.1H163Z" fill="#fff" /><path d="M97 383.2c0-2.7 2-4.8 4.7-4.8v1.6a3.2 3.2 0 0 0-3.1 3.2c0 1.8 1.4 3.2 3 3.2v1.6a4.7 4.7 0 0 1-4.6-4.8ZM101.7 384.8c.8 0 1.5-.7 1.5-1.6 0-.9-.7-1.6-1.5-1.6v3.2Z" fill="#fff" opacity=".5" /><path d="M106.3 383.2c0 2.7-2 4.8-4.6 4.8v-1.6c1.7 0 3-1.4 3-3.2 0-1.8-1.3-3.2-3-3.2v-1.6c2.6 0 4.6 2.1 4.6 4.8ZM101.7 381.6c-.9 0-1.6.7-1.6 1.6 0 .9.7 1.6 1.6 1.6v-3.2Z" fill="#fff" /></g><path stroke="#5DBA14" d="M210.5 350 210.5 370" /><path stroke="#D28200" d="M326.1 295 341.5 307.9" /><g class="chart"><path class="graph-bg" d="M210 40a160 160 0 0 1 0 320 160 160 0 0 1 0-320" /><path class="graph" stroke-dasharray="calc(10.05 * ',
       stroke,
-      '), 1005" d="M210 40a160 160 0 0 1 0 320 160 160 0 0 1 0-320" /></g><g class="risk-ratio"><rect x="242" y="306" width="154" height="82" rx="8" fill="#001828" fill-opacity=".7" /><circle cx="243" cy="326.5" r="4" /><text xml:space="preserve" font-weight="600"><tspan x="255" y="330.7">',
+      rectangle,
       risk,
-      ' RISK</tspan></text><text xml:space="preserve"><tspan x="255" y="355.7">COLLATERAL</tspan><tspan x="255" y="371.7">RATIO ',
-      ratio
+      riskDetail
     );
   }
 
@@ -272,7 +300,7 @@ contract NFTRenderer {
    */
   function _renderBackground(string memory color) internal pure returns (string memory svg) {
     svg = string.concat(
-      '%</tspan></text></g></g><defs><radialGradient id="gradient" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(-133.2 301 119) scale(368.295)"><stop stop-color="',
+      '</g></g><defs><radialGradient id="gradient" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="rotate(-133.2 301 119) scale(368.295)"><stop stop-color="',
       color,
       '" /><stop offset="1" stop-color="',
       color,
@@ -288,11 +316,11 @@ contract NFTRenderer {
     uint256 liquidationRatio,
     uint256 safetyRatio
   ) internal pure returns (string memory, string memory) {
-    if (ratio == 0) return ('NO', '#459d00');
+    if (ratio == 0) return ('NO', '#63676F');
     if (ratio <= liquidationRatio) return ('LIQUIDATION', '#E45200');
     else if (ratio > liquidationRatio && ratio <= safetyRatio) return ('HIGH', '#E45200');
-    else if (ratio > safetyRatio && ratio < (safetyRatio * 120 / 100)) return ('ELEVATED', '#FCBF3B');
-    else return ('LOW', '#459d00');
+    else if (ratio > safetyRatio && ratio <= (safetyRatio * 120 / 100)) return ('ELEVATED', '#FCBF3B');
+    else return ('LOW', '#5DBA14');
   }
 
   /**
