b5f0412602ab93de08fd34299be80325fc246d6e
Hunter King <hking6776@gmail.com>
2023-12-15 18:09:30 -0500

    add Vault721 unit tests

--- test/testnet/e2e/E2ENFT.t.sol
+++ test/testnet/e2e/E2ENFT.t.sol
@@ -4,4 +4,4 @@ pragma solidity 0.8.19;
 import {BaseUser} from '@testnet/scopes/BaseUser.t.sol';
 import {Common, RAD_DELTA} from './Common.t.sol';
 
-contract E2ENFTTest is BaseUser, Common {}
+abstract contract E2ENFTTest is BaseUser, Common {}
