c39e4754f5976ad4c1738e5c6ed65cc7367edf3a
Hunter King <hking6776@gmail.com>
2023-12-15 15:51:59 -0500

    add files

---
+++ test/testnet/e2e/E2ENFT.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
+
+import {BaseUser} from '@testnet/scopes/BaseUser.t.sol';
+import {Common, RAD_DELTA} from './Common.t.sol';
+
+contract E2ENFTTest is BaseUser, Common {}
---
+++ test/testnet/unit/Vault721.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
+
+import {HaiTest, stdStorage, StdStorage} from '@testnet/utils/HaiTest.t.sol';
+
+contract Base is HaiTest {}
+
+contract Unit_Vault721_Constructor is Base {}
