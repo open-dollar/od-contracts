8311ee68b550d536a0270b94b5915a280105ebc2
Hunter King <hking6776@gmail.com>
2024-01-03 10:31:24 -0500

    lint

--- test/testnet/single/SurplusAuctionHouse.t.sol
+++ test/testnet/single/SurplusAuctionHouse.t.sol
@@ -135,7 +135,7 @@ contract SingleBurningSurplusAuctionHouseTest is DSTest {
       ISAFEEngine.SAFEEngineParams({safeDebtCeiling: type(uint256).max, globalDebtCeiling: 0});
     safeEngine = new SAFEEngine(_safeEngineParams);
     protocolToken = new ProtocolToken();
-    protocolToken.initialize("OD Governance", "ODG");
+    protocolToken.initialize('OD Governance', 'ODG');
 
     ISurplusAuctionHouse.SurplusAuctionHouseParams memory _sahParams = ISurplusAuctionHouse.SurplusAuctionHouseParams({
       bidIncrease: 1.05e18,
@@ -264,7 +264,7 @@ contract SingleRecyclingSurplusAuctionHouseTest is DSTest {
       ISAFEEngine.SAFEEngineParams({safeDebtCeiling: type(uint256).max, globalDebtCeiling: 0});
     safeEngine = new SAFEEngine(_safeEngineParams);
     protocolToken = new ProtocolToken();
-    protocolToken.initialize("OD Governance", "ODG");
+    protocolToken.initialize('OD Governance', 'ODG');
 
     ISurplusAuctionHouse.SurplusAuctionHouseParams memory _sahParams = ISurplusAuctionHouse.SurplusAuctionHouseParams({
       bidIncrease: 1.05e18,
@@ -400,7 +400,7 @@ contract SingleMixedStratSurplusAuctionHouseTest is DSTest {
       ISAFEEngine.SAFEEngineParams({safeDebtCeiling: type(uint256).max, globalDebtCeiling: 0});
     safeEngine = new SAFEEngine(_safeEngineParams);
     protocolToken = new ProtocolToken();
-    protocolToken.initialize("OD Governance", "ODG");
+    protocolToken.initialize('OD Governance', 'ODG');
 
     ISurplusAuctionHouse.SurplusAuctionHouseParams memory _sahParams = ISurplusAuctionHouse.SurplusAuctionHouseParams({
       bidIncrease: 1.05e18,
@@ -539,7 +539,7 @@ contract SinglePostSettlementSurplusAuctionHouseTest is DSTest {
       ISAFEEngine.SAFEEngineParams({safeDebtCeiling: type(uint256).max, globalDebtCeiling: 0});
     safeEngine = new SAFEEngine(_safeEngineParams);
     protocolToken = new ProtocolToken();
-    protocolToken.initialize("OD Governance", "ODG");
+    protocolToken.initialize('OD Governance', 'ODG');
 
     IPostSettlementSurplusAuctionHouse.PostSettlementSAHParams memory _pssahParams = IPostSettlementSurplusAuctionHouse
       .PostSettlementSAHParams({bidIncrease: 1.05e18, bidDuration: 3 hours, totalAuctionLength: 2 days});
