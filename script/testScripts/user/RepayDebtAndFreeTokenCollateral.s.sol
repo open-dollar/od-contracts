53e599cd9a7a8b5031c7d1be7c13e835b17399b6
MrDeadCe11 <mister.deadce11@gmail.com>
2024-01-25 20:19:40 -0600

    added missing files

---
+++ script/testScripts/user/RepayDebtAndFreeTokenCollateral.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {TestScripts} from '@script/testScripts/user/utils/TestScripts.s.sol';
+import {SystemCoin} from '@contracts/tokens/SystemCoin.sol';
+ 
+// BROADCAST
+// source .env && forge script RepayDebtAndFreeTokenCollateral --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
+
+// SIMULATE
+// source .env && forge script RepayDebtAndFreeTokenCollateral --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC
+
+contract RepayDebtAndFreeTokenCollateral is TestScripts {
+    /// @dev this script will find the balance of your unlocked collateral and retreive it.
+  function run() public {
+    vm.startBroadcast(vm.envUint('ARB_SEPOLIA_PK'));
+    address proxy = address(deployOrFind(USER2));
+    
+    uint256 sysCoinBalance = systemCoin.balanceOf(address(USER2));
+   systemCoin.approve(proxy, type(uint256).max);
+
+    repayDebtAndFreeTokenCollateral(WSTETH, SAFE, USER2, proxy, (sysCoinBalance - 1000000000000000));
+    vm.stopBroadcast();
+  }
+}
