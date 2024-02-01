10708b48f385426a367de97adec2d163462e1246
0xdavinchee <0xdavinchee@gmail.com>
2024-02-01 21:49:34 +0200

    naming

--- script/testScripts/gov/UpdateTimeDelayAction/ProposeUpdateTimeDelay.s.sol
+++ script/testScripts/gov/UpdateTimeDelayAction/ProposeUpdateTimeDelay.s.sol
@@ -12,7 +12,7 @@ import {NFTRenderer} from '@contracts/proxies/NFTRenderer.sol';
 /// @dev NOTE This script requires the following env vars in the REQUIRED ENV VARS section below
 /// @dev The script will propose setting the time delay value on Vault721
 /// @dev The script will output a JSON file with the proposal data to be used by the QueueProposal and ExecuteProposal scripts
-/// @dev In the root, run: export FOUNDRY_PROFILE=governance && forge script script/testScripts/gov/UpdateTimeDelayAction/UpdateTimeDelay.s.sol
+/// @dev In the root, run: export FOUNDRY_PROFILE=governance && forge script script/testScripts/gov/UpdateTimeDelayAction/ProposeUpdateTimeDelay.s.sol
 contract UpdateTimeDelay is JSONScript {
   function run() public {
     /// REQUIRED ENV VARS ///
