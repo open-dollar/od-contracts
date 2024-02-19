84b88a36465b028ddc051b68bd8700d52960bd0a
Axe <seamac@gmail.com>
2024-02-19 22:36:15 +0000

    selection base on fork

--- test/testnet/e2e/Deploy.t.sol
+++ test/testnet/e2e/Deploy.t.sol
@@ -92,12 +92,20 @@ abstract contract CommonDeploymentTest is ODTest, Deploy {
   }
 
   function test_Grant_Auth() public {
+
     _test_Authorizations(governor, true);
-    if (delegate != address(0)) _test_Authorizations(delegate, true);
-    _test_Authorizations(deployer, false);
+
+    if (delegate != address(0)) {
+     _test_Authorizations(delegate, true);
+    }
+
+    if(!isFork()) { // if not fork, test deployer
+      _test_Authorizations(deployer, false);
+    }
   }
 
   function _test_Authorizations(address _target, bool _permission) internal {
+
     // base contracts
     assertEq(safeEngine.authorizedAccounts(_target), _permission);
     assertEq(oracleRelayer.authorizedAccounts(_target), _permission);
@@ -168,13 +176,16 @@ abstract contract CommonDeploymentTest is ODTest, Deploy {
  *   }
  * }
  */
+
 contract E2EDeploymentSepoliaTest is DeploySepolia, CommonDeploymentTest {
+
   function setUp() public override {
     uint256 forkId = vm.createFork(vm.rpcUrl('sepolia'));
     vm.selectFork(forkId);
 
     create2 = IODCreate2Factory(TEST_CREATE2FACTORY);
     protocolToken = IProtocolToken(SEPOLIA_PROTOCOL_TOKEN);
+
     governor = SEPOLIA_TIMELOCK_CONTROLLER;
     timelockController = TimelockController(payable(SEPOLIA_TIMELOCK_CONTROLLER));
     odGovernor = ODGovernor(payable(SEPOLIA_OD_GOVERNOR));
@@ -201,7 +212,6 @@ contract SepoliaDeploymentTest is SepoliaDeployment, CommonDeploymentTest {
     uint256 forkId = vm.createFork(vm.rpcUrl('sepolia'));
     vm.selectFork(forkId);
 
-    create2 = IODCreate2Factory(TEST_CREATE2FACTORY);
     protocolToken = IProtocolToken(SEPOLIA_PROTOCOL_TOKEN);
     governor = SEPOLIA_TIMELOCK_CONTROLLER;
     timelockController = TimelockController(payable(SEPOLIA_TIMELOCK_CONTROLLER));
