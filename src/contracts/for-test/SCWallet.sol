8c6855c5ee5826665835605d0fcbc9cd47198b09
Hunter King <hking6776@gmail.com>
2024-03-24 09:46:35 -0700

    added safeTransfer unit tests

---
+++ src/contracts/for-test/SCWallet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
+
+import {IERC721Receiver} from '@openzeppelin/token/ERC721/IERC721Receiver.sol';
+
+contract SCWallet is IERC721Receiver {
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) external returns (bytes4) {
+    return IERC721Receiver.onERC721Received.selector;
+  }
+}
+
+contract Bad_SCWallet {
+  function nftReceiver() external returns (uint256) {
+    return 1234;
+  }
+}
