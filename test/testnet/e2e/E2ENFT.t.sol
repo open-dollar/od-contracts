f68c9776c1c42355cebb748dc475a74e4fce5d34
Hunter King <hking6776@gmail.com>
2024-03-26 22:34:13 -0700

    pass nft e2e tests

--- test/testnet/e2e/E2ENFT.t.sol
+++ test/testnet/e2e/E2ENFT.t.sol
@@ -15,13 +15,13 @@ import {Vault721} from '@contracts/proxies/Vault721.sol';
 import {IVault721} from '@interfaces/proxies/IVault721.sol';
 
 contract NFTSetup is Common {
-  uint256 private constant MINT_AMOUNT = 1_000_000 ether;
+  uint256 public constant MINT_AMOUNT = 1000 ether;
 
   address public aliceProxy;
   address public bobProxy;
 
   ERC20ForTest public token;
-  bytes32 constant WSTETH = bytes32('WSTETH');
+  bytes32 constant TKN = bytes32('TKN');
 
   function setUp() public override {
     super.setUp();
@@ -31,6 +31,7 @@ contract NFTSetup is Common {
     vm.label(bobProxy, 'BobProxy');
 
     token = ERC20ForTest(address(collateral[TKN]));
+    token.mint(alice, MINT_AMOUNT);
     vm.stopPrank();
   }
 
@@ -41,13 +42,6 @@ contract NFTSetup is Common {
     } else {
       return proxy;
     }
-  }
-
-  function openSafe(bytes32 _cType, address _usr) public returns (uint256 _safeId) {
-    address _proxy = deployOrFind(_usr);
-    bytes memory payload = abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), _cType, _proxy);
-    bytes memory safeData = ODProxy(_proxy).execute(address(basicActions), payload);
-    _safeId = abi.decode(safeData, (uint256));
   }
 
   function depositCollatAndGenDebt(
@@ -74,131 +68,79 @@ contract E2ENFTTest is NFTSetup {
   using SafeERC20 for IERC20;
 
   function test_openSafe() public {
-    address alice = address(0x420);
-    // uint256 safeId = openSafe(WSTETH, alice);
-    bytes memory payload =
-      abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), WSTETH, aliceProxy);
-
-    vm.prank(alice);
+    vm.startPrank(alice);
+    bytes memory payload = abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), TKN, aliceProxy);
     bytes memory safeData = ODProxy(aliceProxy).execute(address(basicActions), payload);
+    vm.stopPrank();
 
-    // assertEq(safeId, vault721.totalSupply());
+    uint256 safeId = abi.decode(safeData, (uint256));
+    assertEq(safeId, vault721.totalSupply());
 
-    // address safeIdOwner = Vault721(vault721).ownerOf(safeId);
-    // assertEq(safeIdOwner, alice);
+    address safeIdOwner = Vault721(vault721).ownerOf(safeId);
+    assertEq(safeIdOwner, alice);
   }
 
-  // function test_openSafe_lockCollateral_WSTETH() public {
-  //   vm.startPrank(alice);
+  function test_transferSafeToProxyFail() public {
+    vm.startPrank(alice);
+    bytes memory payload = abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), TKN, aliceProxy);
+    bytes memory safeData = ODProxy(aliceProxy).execute(address(basicActions), payload);
+    uint256 safeId = abi.decode(safeData, (uint256));
 
-  //   uint256 safeId = openSafe(WSTETH, alice);
-  //   assertEq(safeId, vault721.totalSupply());
+    vm.expectRevert(Vault721.NotWallet.selector);
+    Vault721(vault721).transferFrom(alice, bobProxy, safeId);
+    vm.stopPrank();
+  }
 
-  //   IERC20(wsteth).approve(aliceProxy, type(uint256).max);
-  //   depositCollatAndGenDebt(WSTETH, vault721.totalSupply(), 0.0001 ether, 0, aliceProxy);
-  //   vm.stopPrank();
-  // }
+  function test_lockCollateral() public {
+    vm.startPrank(alice);
+    bytes memory payload = abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), TKN, aliceProxy);
+    bytes memory safeData = ODProxy(aliceProxy).execute(address(basicActions), payload);
+    uint256 safeId = abi.decode(safeData, (uint256));
 
-  // function test_openSafe_lockCollateral_ARB() public {
-  //   vm.startPrank(alice);
+    token.approve(aliceProxy, type(uint256).max);
+    depositCollatAndGenDebt(TKN, vault721.totalSupply(), 0.0001 ether, 0, aliceProxy);
+    vm.stopPrank();
+  }
 
-  //   uint256 safeId = openSafe(ARB, alice);
-  //   assertEq(safeId, vault721.totalSupply());
+  function test_lockCollateral_generateDebt() public {
+    vm.startPrank(alice);
 
-  //   IERC20(arb).approve(aliceProxy, type(uint256).max);
-  //   depositCollatAndGenDebt(ARB, vault721.totalSupply(), 1 ether, 0, aliceProxy);
-  //   vm.stopPrank();
-  // }
+    bytes memory payload = abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), TKN, aliceProxy);
+    bytes memory safeData = ODProxy(aliceProxy).execute(address(basicActions), payload);
+    uint256 safeId = abi.decode(safeData, (uint256));
 
-  // function test_openSafe_lockCollateral_generateDebt_WSTETH() public {
-  //   vm.startPrank(alice);
+    token.approve(aliceProxy, type(uint256).max);
+    depositCollatAndGenDebt(TKN, vault721.totalSupply(), 1000 ether, 1 ether, aliceProxy);
+    vm.stopPrank();
+  }
 
-  //   uint256 safeId = openSafe(WSTETH, alice);
-  //   assertEq(safeId, vault721.totalSupply());
+  function test_openSafe_lockCollateral_transfer() public {
+    vm.startPrank(alice);
 
-  //   IERC20(wsteth).approve(aliceProxy, type(uint256).max);
-  //   depositCollatAndGenDebt(WSTETH, vault721.totalSupply(), 0.3 ether, 150 ether, aliceProxy);
-  //   vm.stopPrank();
-  // }
+    bytes memory payload = abi.encodeWithSelector(basicActions.openSAFE.selector, address(safeManager), TKN, aliceProxy);
+    bytes memory safeData = ODProxy(aliceProxy).execute(address(basicActions), payload);
+    uint256 safeId = abi.decode(safeData, (uint256));
 
-  // function test_openSafe_lockCollateral_generateDebt_ARB() public {
-  //   vm.startPrank(alice);
+    token.approve(aliceProxy, type(uint256).max);
+    depositCollatAndGenDebt(TKN, vault721.totalSupply(), 0.0001 ether, 0, aliceProxy);
 
-  //   uint256 safeId = openSafe(ARB, alice);
-  //   assertEq(safeId, vault721.totalSupply());
+    uint256 nftBalAliceBefore = Vault721(vault721).balanceOf(alice);
+    uint256 nftBalBobBefore = Vault721(vault721).balanceOf(bob);
 
-  //   IERC20(arb).approve(aliceProxy, type(uint256).max);
-  //   depositCollatAndGenDebt(ARB, vault721.totalSupply(), 125 ether, 75 ether, aliceProxy);
-  //   vm.stopPrank();
-  // }
+    assertEq(nftBalAliceBefore, 1);
+    assertEq(nftBalBobBefore, 0);
 
-  // function test_openSafe_lockCollateral_transfer_WSTETH() public {
-  //   vm.startPrank(alice);
+    Vault721(vault721).transferFrom(alice, bob, vault721.totalSupply());
 
-  //   uint256 safeId = openSafe(WSTETH, alice);
-  //   assertEq(safeId, vault721.totalSupply());
+    uint256 nftBalAliceAfter = Vault721(vault721).balanceOf(alice);
+    uint256 nftBalBobAfter = Vault721(vault721).balanceOf(bob);
 
-  //   IERC20(wsteth).approve(aliceProxy, type(uint256).max);
-  //   depositCollatAndGenDebt(WSTETH, vault721.totalSupply(), 0.0001 ether, 0, aliceProxy);
+    assertEq(nftBalAliceAfter, 0);
+    assertEq(nftBalBobAfter, 1);
+    vm.stopPrank();
 
-  //   uint256 nftBalAliceBefore = Vault721(vault721).balanceOf(alice);
-  //   uint256 nftBalBobBefore = Vault721(vault721).balanceOf(bob);
-
-  //   assertEq(nftBalAliceBefore, 1);
-  //   assertEq(nftBalBobBefore, 0);
-
-  //   Vault721(vault721).transferFrom(alice, bob, vault721.totalSupply());
-
-  //   uint256 nftBalAliceAfter = Vault721(vault721).balanceOf(alice);
-  //   uint256 nftBalBobAfter = Vault721(vault721).balanceOf(bob);
-
-  //   assertEq(nftBalAliceAfter, 0);
-  //   assertEq(nftBalBobAfter, 1);
-  //   vm.stopPrank();
-
-  //   uint256[] memory _safes = safeManager.getSafes(deployOrFind(bob));
-  //   assertEq(_safes.length, 1);
-  //   assertEq(_safes[0], vault721.totalSupply());
-  // }
-
-  // function test_openSafe_lockCollateral_generateDebt_transfer_ARB() public {
-  //   vm.startPrank(alice);
-
-  //   uint256 safeId = openSafe(ARB, alice);
-  //   assertEq(safeId, vault721.totalSupply());
-
-  //   IERC20(arb).approve(aliceProxy, type(uint256).max);
-  //   depositCollatAndGenDebt(ARB, vault721.totalSupply(), 125 ether, 75 ether, aliceProxy);
-
-  //   uint256 nftBalAliceBefore = Vault721(vault721).balanceOf(alice);
-  //   uint256 nftBalBobBefore = Vault721(vault721).balanceOf(bob);
-
-  //   assertEq(nftBalAliceBefore, 1);
-  //   assertEq(nftBalBobBefore, 0);
-
-  //   Vault721(vault721).transferFrom(alice, bob, vault721.totalSupply());
-
-  //   uint256 nftBalAliceAfter = Vault721(vault721).balanceOf(alice);
-  //   uint256 nftBalBobAfter = Vault721(vault721).balanceOf(bob);
-
-  //   assertEq(nftBalAliceAfter, 0);
-  //   assertEq(nftBalBobAfter, 1);
-  //   vm.stopPrank();
-
-  //   uint256[] memory _safes = safeManager.getSafes(deployOrFind(bob));
-  //   assertEq(_safes.length, 1);
-  //   assertEq(_safes[0], vault721.totalSupply());
-  // }
-
-  // todo: uncomment after next Sepolia deployment
-  // function test_openSafe_transferToProxyFail() public {
-  //   vm.startPrank(alice);
-
-  //   uint256 safeId = openSafe(ARB, alice);
-  //   assertEq(safeId, vault721.totalSupply());
-
-  //   vm.expectRevert(Vault721.NotWallet.selector);
-  //   Vault721(vault721).transferFrom(alice, bobProxy, safeId);
-  //   vm.stopPrank();
-  // }
+    uint256[] memory _safes = safeManager.getSafes(deployOrFind(bob));
+    assertEq(_safes.length, 1);
+    assertEq(_safes[0], vault721.totalSupply());
+  }
 }
