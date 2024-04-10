025681adfc3dda99a2ac8dc2b7c348aca349e815
Axe <seamac@gmail.com>
2024-04-10 19:38:32 +0100

    update helper

--- test/utils/BytesDecoder.sol
+++ test/utils/BytesDecoder.sol
@@ -6,11 +6,19 @@ contract BytesDecoder {
     return abi.decode(data, (uint256));
   }
 
+  function decodeAsInt256(bytes memory data) public pure returns (int256) {
+    return abi.decode(data, (int256));
+  }
+
   function decodeAsAddress(bytes memory data) public pure returns (address) {
     return abi.decode(data, (address));
   }
 
   function decodeAsBytes32(bytes memory data) public pure returns (bytes32) {
     return abi.decode(data, (bytes32));
+  }
+
+  function decodeAsBool(bytes memory data) public pure returns (bool) {
+    return abi.decode(data, (bool));
   }
 }
