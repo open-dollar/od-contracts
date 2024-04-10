5cbdc0863d9cd4e05d1cfbb0876343976f188ab0
Axe <seamac@gmail.com>
2024-04-10 14:05:07 +0100

    add more tests

--- test/mocks/CollateralAuctionHouseFactoryForTest.sol
+++ test/mocks/CollateralAuctionHouseFactoryForTest.sol
@@ -7,6 +7,7 @@ import {
   EnumerableSet
 } from '@contracts/factories/CollateralAuctionHouseFactory.sol';
 import {ICollateralAuctionHouse} from '@interfaces/ICollateralAuctionHouse.sol';
+import { CollateralAuctionHouseChild } from '@contracts/factories/CollateralAuctionHouseChild.sol';
 
 contract CollateralAuctionHouseFactoryForTest is CollateralAuctionHouseFactory {
   using EnumerableSet for EnumerableSet.Bytes32Set;
@@ -23,5 +24,19 @@ contract CollateralAuctionHouseFactoryForTest is CollateralAuctionHouseFactory {
 
   function setContractEnabled(bool _contractEnabled) external {
     contractEnabled = _contractEnabled;
+  }
+
+  function callChildDisable(address child) external {
+    CollateralAuctionHouseChild(child).disableContract();
+  }
+
+  function callChildSetOracle(address oracle) external {
+    bytes memory oracleAsBytes = abi.encode(oracle);
+    _modifyParameters('oracleRelayer',oracleAsBytes);
+  }
+
+  function callChildSetLiquidationEngine(address liqEngine) external {
+    bytes memory liqEngineAsBytes = abi.encode(liqEngine);
+    _modifyParameters('liquidationEngine',liqEngineAsBytes);
   }
 }
