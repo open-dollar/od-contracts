0ac610f378f20fbecf0ac8fa0d37c05bcf2141fa
Axe <seamac@gmail.com>
2024-04-11 22:32:58 +0100

    Auto stash before merge of "353" and "origin/dev"

--- test/utils/BytesDecoder.sol
+++ test/utils/BytesDecoder.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 contract BytesDecoder {
   function decodeAsUint256(bytes memory data) public pure returns (uint256) {
