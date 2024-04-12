203c90953dc2b2f5a55262b4361076d7498b8bec
Axe <seamac@gmail.com>
2024-04-12 16:00:22 +0100

    more tests

--- test/unit/proxies/actions/BasicActions.t.sol
+++ test/unit/proxies/actions/BasicActions.t.sol
@@ -7,11 +7,55 @@ import {BasicActions} from '@contracts/proxies/actions/BasicActions.sol';
 import {ODSafeManagerMock} from './GlobalSettlementActions.t.sol';
 import {CoinJoinMock} from './CollateralBidActions.t.sol';
 import {SafeEngineMock} from './SurplusBidActions.t.sol';
+import {IERC20Metadata} from '@openzeppelin/token/ERC20/extensions/IERC20Metadata.sol';
+
+contract CollateralJoinMock {
+  bool public wasJoinCalled;
+  bool public wasExitCalled;
+  address public collateralToken;
+  address public safeEngine;
+
+  function reset() external {
+    wasJoinCalled = false;
+    wasExitCalled = false;
+    collateralToken = address(0);
+    safeEngine = address(0);
+  }
+
+  function _mock_setCollateralToken(address _collateralToken) external {
+    collateralToken = _collateralToken;
+  }
+
+  function _mock_setSafeEngine(address _safeEngine) external {
+    safeEngine = _safeEngine;
+  }
+
+  function collateral() external view returns (IERC20Metadata _collateral) {
+    return IERC20Metadata(collateralToken);
+  }
+
+  function join(address _account, uint256 _wei) external {
+    wasJoinCalled = true;
+  }
+
+  function systemCoin() external view returns (address) {
+    return collateralToken;
+  }
+
+  function decimals() external view returns (uint256) {
+    return 18;
+  }
+
+  function exit(address _account, uint256 _wei) external {
+    wasExitCalled = true;
+  }
+}
 
 contract BasicActionsTest is ActionBaseTest {
   BasicActions basicActions = new BasicActions();
   ODSafeManagerMock safeManager = new ODSafeManagerMock();
   CoinJoinMock coinJoin = new CoinJoinMock();
+  CollateralJoinMock collateralJoin = new CollateralJoinMock();
 
   function setUp() public {
     proxy = new ODProxy(alice);
@@ -180,5 +224,139 @@ contract BasicActionsTest is ActionBaseTest {
     );
 
     assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_lockTokenCollateral() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    collateralJoin._mock_setCollateralToken(address(coinJoin.systemCoin()));
+
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature(
+        'lockTokenCollateral(address,address,uint256,uint256)', address(safeManager), address(collateralJoin), 1, 1
+      )
+    );
+
+    assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_repayAllDebt() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    SafeEngineMock(safeManager.safeEngine())._mock_setCollateralData(0, 0, 1, 0, 0);
+
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature('repayAllDebt(address,address,uint256)', address(safeManager), address(coinJoin), 1)
+    );
+
+    assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_lockTokenCollateralAndGenerateDebt() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    collateralJoin._mock_setCollateralToken(address(coinJoin.systemCoin()));
+    SafeEngineMock(safeManager.safeEngine())._mock_setCollateralData(0, 0, 1, 0, 0);
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature(
+        'lockTokenCollateralAndGenerateDebt(address,address,address,uint256,uint256,uint256)',
+        address(safeManager),
+        address(collateralJoin),
+        address(coinJoin),
+        1,
+        1,
+        1
+      )
+    );
+
+    assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_openLockTokenCollateralAndGenerateDebt() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    collateralJoin._mock_setCollateralToken(address(coinJoin.systemCoin()));
+    SafeEngineMock(safeManager.safeEngine())._mock_setCollateralData(0, 0, 1, 0, 0);
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature(
+        'openLockTokenCollateralAndGenerateDebt(address,address,address,bytes32,uint256,uint256)',
+        address(safeManager),
+        address(collateralJoin),
+        address(coinJoin),
+        0,
+        1,
+        1
+      )
+    );
+
+    assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_repayDebtAndFreeTokenCollateral() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    collateralJoin._mock_setCollateralToken(address(coinJoin.systemCoin()));
+    SafeEngineMock(safeManager.safeEngine())._mock_setCollateralData(0, 0, 1, 0, 0);
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature(
+        'repayDebtAndFreeTokenCollateral(address,address,address,uint256,uint256,uint256)',
+        address(safeManager),
+        address(coinJoin),
+        address(collateralJoin),
+        1,
+        1,
+        1
+      )
+    );
+
+    assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_repayAllDebtAndFreeTokenCollateral() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    collateralJoin._mock_setCollateralToken(address(coinJoin.systemCoin()));
+    SafeEngineMock(safeManager.safeEngine())._mock_setCollateralData(0, 0, 1, 0, 0);
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature(
+        'repayAllDebtAndFreeTokenCollateral(address,address,address,uint256,uint256)',
+        address(safeManager),
+        address(coinJoin),
+        address(collateralJoin),
+        1,
+        1
+      )
+    );
+
+    assertTrue(safeManager.wasModifySAFECollateralizationCalled());
+  }
+
+  function test_collectTokenCollateral() public {
+    safeManager.reset();
+    vm.startPrank(alice);
+    coinJoin.systemCoin().approve(address(proxy), 10 ether);
+    collateralJoin._mock_setCollateralToken(address(coinJoin.systemCoin()));
+    collateralJoin._mock_setSafeEngine(safeManager.safeEngine());
+    SafeEngineMock(safeManager.safeEngine())._mock_setCollateralData(0, 0, 1, 0, 0);
+
+    proxy.execute(
+      address(basicActions),
+      abi.encodeWithSignature(
+        'collectTokenCollateral(address,address,uint256,uint256)', address(safeManager), address(collateralJoin), 1, 1
+      )
+    );
+    assertTrue(collateralJoin.wasExitCalled());
   }
 }
