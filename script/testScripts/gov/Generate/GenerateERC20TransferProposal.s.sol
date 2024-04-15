174ff8175b9b777a89c27c036cdc5af7ef93e1ff
MrDeadCe11 <mister.deadce11@gmail.com>
2024-04-15 16:29:33 -0500

    added erc20 transfer script

--- script/testScripts/gov/Generate/GenerateERC20TransferProposal.s.sol
+++ script/testScripts/gov/Generate/GenerateERC20TransferProposal.s.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.20;
 import {JSONScript} from '@script/testScripts/gov/helpers/JSONScript.s.sol';
 import {ODGovernor} from '@contracts/gov/ODGovernor.sol';
 import {IERC20} from '@openzeppelin/token/ERC20/IERC20.sol';
+import {Generator} from '@script/testScripts/gov/Generator.s.sol';
+import 'forge-std/StdJson.sol';
 
 /// @title ProposeAddCollateral Script
 /// @author OpenDollar
@@ -12,16 +14,25 @@ import {IERC20} from '@openzeppelin/token/ERC20/IERC20.sol';
 /// @dev This script is used to propose transfer from the governance contract to a specific account
 /// @dev The script will output a JSON file with the proposal data to be used by the QueueProposal and ExecuteProposal scripts
 /// @dev In the root, run: export FOUNDRY_PROFILE=governance && forge script --rpc-url <RPC_URL> script/testScripts/gov/ERC20TransferAction/ProposeERC20Transfer.s.sol
-contract ProposeERC20Transfer is JSONScript {
-  function run() public {
-    /// REQUIRED ENV VARS ///
-    address governanceAddress = vm.envAddress('GOVERNANCE_ADDRESS');
-    address ERC20TokenAddress = vm.envAddress('ERC20_TRANSFER_TOKEN_ADDRESS');
-    address receiverAddress = vm.envAddress('ERC20_TRANSFER_RECEIVER_ADDRESS');
-    address fromAddress = vm.envAddress('ERC20_TRANSFER_FROM_ADDRESS');
-    uint256 amountToTransfer = vm.envUint('ERC20_TRANSFER_AMOUNT');
-    /// END REQUIRED ENV VARS ///
+contract GenerateERC20TransferProposal is Generator, JSONScript {
+  using stdJson for string;
 
+  string public objectKey = 'PROPOSE_ERC20_TRANSFER_KEY';
+  address public governanceAddress;
+  address public ERC20TokenAddress;
+  address public receiverAddress;
+  address public fromAddress;
+  uint256 public amountToTransfer;
+
+  function _loadBaseData(string memory json) internal override {
+    governanceAddress = json.readAddress(string(abi.encodePacked('.odGovernor')));
+    ERC20TokenAddress = json.readAddress(string(abi.encodePacked('.erc20Token')));
+    receiverAddress = json.readAddress(string(abi.encodePacked('.transferTo')));
+    fromAddress = json.readAddress(string(abi.encodePacked('.transferFrom')));
+    amountToTransfer = json.readUint('.amount');
+  }
+
+  function _generateProposal() internal override {
     ODGovernor gov = ODGovernor(payable(governanceAddress));
     address[] memory targets = new address[](1);
     {
@@ -41,29 +52,19 @@ contract ProposeERC20Transfer is JSONScript {
     string memory description = 'Transfer ERC20 tokens to receiver';
     bytes32 descriptionHash = keccak256(bytes(description));
 
-    vm.startBroadcast(vm.envUint('GOV_EXECUTOR_PK'));
+    vm.startBroadcast(privateKey);
 
     // Propose the action to add transfer the ERC20 token to the receiver
     uint256 proposalId = gov.propose(targets, values, calldatas, description);
-    string memory stringProposalId = vm.toString(proposalId);
+    string memory stringProposalId = vm.toString(proposalId / 10 ** 69);
 
     // Verify the proposalId is expected
     assert(proposalId == gov.hashProposal(targets, values, calldatas, descriptionHash));
 
     {
-      string memory ERC20TokenAddressAsString = vm.toString(ERC20TokenAddress);
-      string memory fromAddressAsString = vm.toString(fromAddress);
-      string memory receiverAddressAsString = vm.toString(receiverAddress);
-
-      // Build the JSON output
-      string memory objectKey = 'PROPOSE_ERC20_TRANSFER_KEY';
-      vm.serializeString(objectKey, 'tokenAddress', ERC20TokenAddressAsString);
-      vm.serializeString(objectKey, 'fromAddress', fromAddressAsString);
-      vm.serializeString(objectKey, 'toAddress', receiverAddressAsString);
-      vm.serializeUint(objectKey, 'amountToTransferInWei', amountToTransfer);
       string memory jsonOutput =
         _buildProposalParamsJSON(proposalId, objectKey, targets, values, calldatas, description, descriptionHash);
-      vm.writeJson(jsonOutput, string.concat('./gov-output/', stringProposalId, '-transfer-tokens.json'));
+      vm.writeJson(jsonOutput, string.concat('./gov-output/', network, '/', stringProposalId, '-transfer-erc20.json'));
     }
 
     // Expected JSON output:
@@ -81,5 +82,9 @@ contract ProposeERC20Transfer is JSONScript {
     // }
 
     vm.stopBroadcast();
+  }
+
+  function _serializeCurrentJson(string memory _objectKey) internal override returns (string memory _serializedInput) {
+    _serializedInput = vm.serializeJson(_objectKey, json);
   }
 }
