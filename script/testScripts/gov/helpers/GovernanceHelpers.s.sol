d29c938fc372b619e3779ec3b9d16d6d1b9fa66a
MrDeadCe11 <mister.deadce11@gmail.com>
2024-04-20 18:51:41 -0500

    fixed modify parameters script

---
+++ script/testScripts/gov/helpers/GovernanceHelpers.s.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+//solhint-disable
+
+import 'forge-std/Script.sol';
+import 'forge-std/console2.sol';
+import {AnvilDeployment} from '@script/anvil/deployment/AnvilDeployment.t.sol';
+import {ForkManagement} from '@script/testScripts/gov/helpers/ForkManagement.s.sol';
+import {ODGovernor} from '@contracts/gov/ODGovernor.sol';
+import {IGovernor} from '@openzeppelin/governance/IGovernor.sol';
+import {IVotes} from '@openzeppelin/governance/utils/IVotes.sol';
+
+contract GovernanceHelpers is Script, ForkManagement {
+  using stdJson for string;
+
+  IGovernor.ProposalState public propState;
+  ODGovernor public gov;
+  uint256[] public values;
+  address[] public targets;
+  bytes[] public calldatas;
+  string public description;
+  bytes32 public descriptionHash;
+  uint256 public proposalId;
+  address protocolToken;
+
+  function _loadBaseData() internal virtual {
+    values = json.readUintArray(string(abi.encodePacked('.values')));
+    targets = json.readAddressArray(string(abi.encodePacked('.targets')));
+    calldatas = json.readBytesArray(string(abi.encodePacked('.calldatas')));
+    description = json.readString(string(abi.encodePacked('.description')));
+    descriptionHash = json.readBytes32(string(abi.encodePacked('.descriptionHash')));
+    proposalId = json.readUint(string(abi.encodePacked('.proposalId')));
+    gov = ODGovernor(payable(json.readAddress(string(abi.encodePacked(('.odGovernor'))))));
+    protocolToken = json.readAddress(string(abi.encodePacked('.protocolToken')));
+  }
+
+  function run(string memory _filePath) public {
+    _loadJson(_filePath);
+    _checkNetworkParams();
+    _loadPrivateKeys();
+    _loadBaseData();
+
+    vm.startBroadcast(_privateKey);
+    _Vote();
+  }
+
+  function delegateTokens(string memory _filePath) public {
+    _loadJson(_filePath);
+    _checkNetworkParams();
+    _loadPrivateKeys();
+    _loadBaseData();
+
+    vm.startBroadcast(_privateKey);
+    IVotes(protocolToken).delegate(proposer);
+    uint256 voteWeight = IVotes(protocolToken).getVotes(proposer);
+    console2.log('Current vote weight: ', voteWeight);
+  }
+
+  function _Vote() internal {
+    console2.log('Voting Delay:', gov.votingDelay());
+    console2.log('Voting Period:', gov.votingPeriod());
+    console2.log('Voting weight: ', IVotes(address(protocolToken)).getVotes(proposer));
+
+    propState = gov.state(proposalId);
+    if (propState == IGovernor.ProposalState.Active) {
+      console2.log('#######################################');
+      console2.log('Casting Vote...');
+      gov.castVote(proposalId, 1);
+
+      _getPropInfo(proposalId);
+      _logPropState(propState);
+    } else {
+      console2.log('#######################################');
+      console2.log('Vote not active!');
+      _logPropState(propState);
+      _getPropInfo(proposalId);
+    }
+  }
+
+  function _getPropInfo(uint256 propId) internal {
+    (
+      uint256 id,
+      address proposer,
+      ,
+      uint256 startBlock,
+      uint256 endBlock,
+      uint256 forVotes,
+      uint256 againstVotes,
+      uint256 abstainVotes,
+      ,
+    ) = gov.proposals(propId);
+    console2.log('PROPID: ', id);
+    console2.log('PROPOSER: ', proposer);
+    console2.log('START BLOCK: ', startBlock);
+    console2.log('END BLOCK: ', endBlock);
+    console2.log('FOR VOTES: ', forVotes);
+    console2.log('AGAINST VOTES: ', againstVotes);
+    console2.log('ABSTAIN VOTES: ', abstainVotes);
+  }
+
+  function _logPropState(IGovernor.ProposalState _state) internal {
+    if (uint8(_state) == 0) {
+      console2.log('Prop state: Pending');
+    } else if (uint8(_state) == 1) {
+      console2.log('Prop state: Active');
+    } else if (uint8(_state) == 2) {
+      console2.log('Prop state: Canceled');
+    } else if (uint8(_state) == 3) {
+      console2.log('Prop state: Defeated');
+    } else if (uint8(_state) == 4) {
+      console2.log('Prop state: Succeeded');
+    } else if (uint8(_state) == 5) {
+      console2.log('Prop state: Queued');
+    } else if (uint8(_state) == 6) {
+      console2.log('Prop state: Expired');
+    } else if (uint8(_state) == 7) {
+      console2.log('Prop state: Executed');
+    }
+  }
+}
---
+++ tasks/governanceManager.sh
@@ -0,0 +1,289 @@
+#!/bin/bash
+source .env
+
+set -e
+
+######################
+## GLOBAL VARIABLES ##
+######################
+
+RPC_ENDPOINT=""
+PRIVATE_KEY=""
+
+###############
+## FUNCTIONS ##
+###############
+
+function delimitier() {
+  echo '#################################################'
+}
+
+function vote() {
+  declare OUTPUT=($(node ./tasks/parseNetwork.js $1))
+  NETWORK=${OUTPUT[0]}
+  CAST_PATH=${OUTPUT[1]}
+  CALLDATA=$(cast calldata "run(string)" $CAST_PATH)
+
+  getRpcAndPk $NETWORK
+
+  echo "Simulating... "
+
+  simulate "GovernanceHelpers" $CALLDATA $ANVIL_RPC $PRIVATE_KEY
+
+  delimitier
+  echo "VOTING"
+  read -p "Please verify the data and confirm that you want to Vote on this proposal (y/n): " CONFIRMATION
+
+  if [[ $CONFIRMATION == "y" || $CONFIRMATION == "Y" ]]; then
+    echo "Voting for proposal on $NETWORK..."
+    broadcast "GovernanceHelpers" $CALLDATA $ANVIL_RPC $PRIVATE_KEY
+  fi
+}
+
+function queue() {
+  declare OUTPUT=($(node ./tasks/parseNetwork.js $1))
+  NETWORK=${OUTPUT[0]}
+  CAST_PATH=${OUTPUT[1]}
+  CALLDATA=$(cast calldata "run(string)" $CAST_PATH)
+
+  getRpcAndPk $NETWORK
+
+  simulate "QueueProposal" $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+
+  delimitier
+  echo "QUEUEING"
+  read -p "Please verify the data and confirm that you want to queue this proposal (y/n):" CONFIRMATION
+
+  if [[ $CONFIRMATION == "y" || $CONFIRMATION == "Y" ]]; then
+    echo "Passing proposal on Anvil..."
+    broadcast "QueueProposal" $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+  fi
+}
+
+function generateProposal() {
+  declare OUTPUT=($(node ./tasks/parseNetwork.js $1))
+  NETWORK=${OUTPUT[0]}
+  CAST_PATH=${OUTPUT[1]}
+
+  getRpcAndPk $NETWORK
+
+  CALLDATA=$(cast calldata "run(string)" $CAST_PATH)
+
+  COMMAND_PATH=$(node tasks/parseProposalPath.js $1)
+
+  CALLDATA=$(cast calldata "run(string)" $CAST_PATH)
+
+  simulate $COMMAND_PATH $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+}
+
+function delegate() {
+  declare OUTPUT=($(node ./tasks/parseNetwork.js $1))
+  NETWORK=${OUTPUT[0]}
+  CAST_PATH=${OUTPUT[1]}
+  getRpcAndPk $NETWORK
+
+  CALLDATA=$(cast calldata "delegateTokens(string)" $CAST_PATH)
+
+  simulate "GovernanceHelpers" $CALLDATA $ANVIL_RPC $PRIVATE_KEY
+  delimitier
+  echo "DELEGATING"
+  read -p "Please verify the data and confirm that you want to delegate your tokens (y/n):" CONFIRMATION
+  if [[ $CONFIRMATION == "y" || $CONFIRMATION == "Y" ]]; then
+    echo "Passing proposal on Anvil..."
+    broadcast "GovernanceHelpers" $CALLDATA $ANVIL_RPC $PRIVATE_KEY
+
+  fi
+}
+
+function simulate() {
+  forge script $1 -s $2 --rpc-url $3 --private-key $4
+}
+
+function broadcast() {
+  forge script $1 -s $2 --rpc-url $3 --private-key $4 --broadcast
+}
+
+function display_help() {
+  echo ""
+  echo " Manage your governance proposals!"
+  echo ""
+  echo " The correct proposing and voting order is:"
+  echo " 1. Generate the proposal."
+  echo " 2. Delegate your tokens."
+  echo " 3. Submit your proposal."
+  echo " 4. After enough time has passed, Vote on your proposal."
+  echo " 5. If your proposal passes you can queue it to be executed."
+  echo " 6. After enough time has passed you can execute it."
+
+  echo " $(tput smul)Usage:$(tput sgr0) yarn propose [option flag] [proposal Path]"
+  echo ""
+  echo " $(tput smul)Options:$(tput sgr0) "
+  echo "-h, --help                        Print help"
+  echo "-g, --generate                    Generate your proposal from the simple input json             |  example: propose -g gov-input/anvil/new-ModifyParameters.json"
+  echo "-d, --delegate                    Delegate your votes uses gov-output path                      |  example: propose -d gov-output/anvil/38642346-modifyParameters.json"
+  echo "-s, --submit                      Submit your proposal with the generated gov-output path       |  example: propose -s gov-output/anvil/38642346-modifyParameters.json"
+  echo "-v, --vote                        Vote for your submitted proposal                              |  example: propose -v gov-output/anvil/38642346-modifyParameters.json"
+  echo "-q, --queue                       Queue your passed proposal                                    |  example: propose -q gov-output/anvil/38642346-modifyParameters.json"
+  echo "-x, --execute                     Execute your queued proposal                                  |  example: propose -x gov-output/anvil/38642346-modifyParameters.json"
+  echo ""
+  echo " $(tput smul)Anvil only commands:$(tput sgr0) "
+  echo "-r, --rpc-call                    Make an rpc call make sure to add any necessary arguments     |  example: propose -r anvil_mine 2"
+  echo "-sve, --submit-vote-execute       Submit, mine, vote, mine, queue, mine, execute.               |  example: propose -sve gov-output/anvil/38642346-modifyParameters.json"
+  echo ""
+  exit 0
+}
+
+function submit() {
+  declare OUTPUT=($(node ./tasks/parseNetwork.js $1))
+  NETWORK=${OUTPUT[0]}
+  CAST_PATH=${OUTPUT[1]}
+  CALLDATA=$(cast calldata "run(string)" $CAST_PATH)
+  getRpcAndPk $NETWORK
+
+  FOUNDRY_PROFILE=governance
+  simulate "Proposer" $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+  delimitier
+  echo " SUBMITTING"
+  read -p "Please verify the data and confirm the submission of this proposal (y/n):" CONFIRMATION
+
+  if [[ $CONFIRMATION == "y" || $CONFIRMATION == "Y" ]]; then
+    echo "Executing..."
+    FOUNDRY_PROFILE=governance
+    broadcast "Proposer" $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+
+  fi
+}
+
+function makeCall() {
+  node ./tasks/anvilRPCCalls.js $1 $2
+}
+
+function getRpcAndPk() {
+  if [[ $1 = "arb-sepolia" || $1 = "sepolia" ]]; then
+    RPC_ENDPOINT=$ARB_SEPOLIA_RPC PRIVATE_KEY=$ARB_SEPOLIA_PK
+  elif [[ $1 = "anvil" ]]; then
+    RPC_ENDPOINT=$ANVIL_RPC PRIVATE_KEY=$ANVIL_ONE
+  elif [[ $1 = "arb-mainnet" || $1 = "mainnet" ]]; then
+    RPC_ENDPOINT=$ARB_MAINNET_RPC PRIVATE_KEY=$ARB_MAINNET_PK
+  else
+    echo "Unrecognized target environment"
+    exit 1
+  fi
+}
+
+function execute() {
+  declare OUTPUT=($(node ./tasks/parseNetwork.js $1))
+  NETWORK=${OUTPUT[0]}
+  CAST_PATH=${OUTPUT[1]}
+  CALLDATA=$(cast calldata "run(string)" $CAST_PATH)
+  getRpcAndPk $NETWORK
+  FOUNDRY_PROFILE=governance
+  simulate "Executor" $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+  delimitier
+  echo "EXECUTING"
+  read -p "Please verify the data and confirm the execution of this proposal (y/n):" CONFIRMATION
+
+  if [[ $CONFIRMATION == "y" || $CONFIRMATION == "Y" ]]; then
+    echo "Executing..."
+    FOUNDRY_PROFILE=governance
+    broadcast "Executor" $CALLDATA $RPC_ENDPOINT $PRIVATE_KEY
+
+  fi
+}
+
+function checkPath() {
+  if [[ $1 == "" ]]; then
+    display_help
+  fi
+}
+
+function submitVoteAndExecute() {
+  makeCall "anvil_mine" 2
+  submit $1
+  makeCall "anvil_mine" 2
+  vote $1
+  makeCall "anvil_mine" 16
+  queue $1
+  makeCall "anvil_mine" 61
+  execute $1
+}
+
+while :; do
+  case "$1" in
+  "")
+    display_help
+    ;;
+  -h | --help)
+    display_help
+    ;;
+
+  -a | --add-options)
+    # do something here call function
+    # and write it in your help function display_help()
+    shift 2
+    ;;
+  -r | --rpc-call)
+    checkPath $2
+    makeCall $2 $3
+    exit 0
+    ;;
+  -d | --delegate)
+    checkPath $2
+    delegate $2
+    exit 0
+    ;;
+  -q | --queue)
+    checkPath $2
+    queue $2
+    exit 0
+    ;;
+  -s | --submit)
+    checkPath $2
+    submit $2
+    exit 0
+    ;;
+  -v | --vote)
+    checkPath $2
+    vote $2
+    exit 0
+    ;;
+  -g | --generate)
+    checkPath $2
+    generateProposal $2
+    exit 0
+    ;;
+  -x | --execute)
+    checkPath $2
+    execute $2
+    exit 0
+    ;;
+  -sve | --submit-vote-execute)
+    checkPath $2
+    submitVoteAndExecute $2
+    exit 0
+    ;;
+  --) # End of all options
+    shift
+    break
+    ;;
+  -*)
+    echo "Error: Unknown option: $1" >&2
+    ## or call function display_help
+    exit 1
+    ;;
+  *) # No more options
+    break
+    ;;
+  esac
+done
+
+#######################
+# Check if parameter  #
+# is set then execute #
+#######################
+
+if [[ $1 != "" && $2 == "" ]]; then
+  display_help $1
+fi
+
+exit 0
