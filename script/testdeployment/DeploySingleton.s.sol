6ddb71daec14aa84580454c49b9268604343b55c
huntrr <50965054+daopunk@users.noreply.github.com>
2024-04-26 11:04:01 -0700

    add proxy singleton script (#657)
    * add script
    
    * add proxy var

--- script/testdeployment/DeploySingleton.s.sol
+++ script/testdeployment/DeploySingleton.s.sol
@@ -5,14 +5,29 @@ import {Script} from 'forge-std/Script.sol';
 import {OpenDollarGovernance, ProtocolToken, IProtocolToken} from '@contracts/tokens/ProtocolToken.sol';
 import {OpenDollar, SystemCoin, ISystemCoin} from '@contracts/tokens/SystemCoin.sol';
 import {Vault721} from '@contracts/proxies/Vault721.sol';
+import {ODProxy} from '@contracts/proxies/ODProxy.sol';
 
 // BROADCAST
-// source .env && forge script DeploySingletonMain --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
+// source .env && forge script DeployProxySingletonMain --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
 
 // SIMULATE
-// source .env && forge script DeploySingletonMain --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC
+// source .env && forge script DeployProxySingletonMain --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC
 
-contract DeploySingletonMain is Script {
+contract DeployProxySingletonMain is Script {
+  function run() public {
+    vm.startBroadcast(vm.envUint('ARB_MAINNET_DEPLOYER_PK'));
+    new ODProxy(msg.sender);
+    vm.stopBroadcast();
+  }
+}
+
+// BROADCAST
+// source .env && forge script DeployAllSingletonMain --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
+
+// SIMULATE
+// source .env && forge script DeployAllSingletonMain --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC
+
+contract DeployAllSingletonMain is Script {
   function run() public {
     vm.startBroadcast(vm.envUint('ARB_MAINNET_DEPLOYER_PK'));
     new OpenDollarGovernance();
@@ -27,12 +42,12 @@ contract DeploySingletonMain is Script {
  */
 
 // BROADCAST
-// source .env && forge script DeploySingletonSepolia --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
+// source .env && forge script DeployAllSingletonSepolia --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
 
 // SIMULATE
-// source .env && forge script DeploySingletonSepolia --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC
+// source .env && forge script DeployAllSingletonSepolia --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_SEPOLIA_RPC
 
-contract DeploySingletonSepolia is Script {
+contract DeployAllSingletonSepolia is Script {
   function run() public {
     vm.startBroadcast(vm.envUint('ARB_SEPOLIA_DEPLOYER_PK'));
     new OpenDollarGovernance();
