163c6b632a79373fd232ecfc4c90cac934df204c
Axe <seamac@gmail.com>
2024-04-27 15:23:56 +0100

    add saviour gas burner

--- test/unit/LiquidationEngine.t.sol
+++ test/unit/LiquidationEngine.t.sol
@@ -19,6 +19,16 @@ import {StdStorage, stdStorage} from 'forge-std/StdStorage.sol';
 import {Math, MAX_RAD, RAY, WAD} from '@libraries/Math.sol';
 import {Assertions} from '@libraries/Assertions.sol';
 
+// Mock that will burn all gas
+contract MockFatSaviour is ISAFESaviour {
+  event Log(string);
+  function saveSAFE(address, bytes32, address) external returns (bool, uint256, uint256) {
+    emit Log('FatSaviour called');
+    while(true) {} // this loop will consume all available gas
+    return (true, 0, 0);
+  }
+}
+
 abstract contract Base is ODTest {
   using stdStorage for StdStorage;
 
@@ -743,6 +753,7 @@ contract Unit_LiquidationEngine_LiquidateSafe is Base {
   event UpdateCurrentOnAuctionSystemCoins(uint256 _currentOnAuctionSystemCoins);
   event SaveSAFE(bytes32 indexed _cType, address indexed _safe, uint256 _collateralAddedOrDebtRepaid);
   event FailedSAFESave(bytes _failReason);
+  event Log(string _msg);
 
   struct Liquidation {
     uint256 accumulatedRate;
@@ -1349,6 +1360,16 @@ contract Unit_LiquidationEngine_LiquidateSafe is Base {
       auctionId
     );
 
+    liquidationEngine.liquidateSAFE(collateralType, safe);
+  }
+
+  function test_Emit_Liquidate_withRougeSaviour(Liquidation memory _liquidation) public happyPathFullLiquidation(_liquidation) {
+    MockFatSaviour _testSaveSaviour = new MockFatSaviour();
+    _mockChosenSafeSaviour(collateralType, safe, address(_testSaveSaviour));
+    _mockSafeSaviours(address(_testSaveSaviour), 1);
+    vm.prank(user);
+    vm.expectEmit();
+    emit Log('FatSaviour called');
     liquidationEngine.liquidateSAFE(collateralType, safe);
   }
 
