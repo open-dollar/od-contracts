c010be137bc2ea25a016829f941bae2def072686
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-01 13:31:55 -0700

    Add UpdateTimelockMinDelay

--- script/postdeployment/UpdateParams.s.sol
+++ script/postdeployment/UpdateParams.s.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.20;
 import '@script/Registry.s.sol';
 import {Script} from 'forge-std/Script.sol';
 import {MainnetDeployment} from '@script/MainnetDeployment.s.sol';
+import {TimelockController} from '@openzeppelin/governance/TimelockController.sol';
+import 'forge-std/console2.sol';
 
 /**
  * @dev update to desired values:
@@ -63,5 +65,37 @@ contract UpdateLiquidationEngineParams is Base {
     bytes memory data = abi.encode(_NEW_GAS_LIMIT);
 
     liquidationEngine.modifyParameters('saviourGasLimit', data);
+  }
+}
+
+// BROADCAST
+// source .env && forge script UpdateTimelockMinDelay --skip-simulation --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC --broadcast --verify --etherscan-api-key $ARB_ETHERSCAN_API_KEY
+
+// SIMULATE
+// source .env && forge script UpdateTimelockMinDelay --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC
+
+contract UpdateTimelockMinDelay is Base {
+  function run() public prankSwitch(_broadcast) {
+    uint256 _newDelay = 3600;
+
+    address[] memory targets = new address[](1);
+    {
+      targets[0] = address(timelockController);
+    }
+    uint256[] memory values = new uint256[](1);
+    {
+      values[0] = 0;
+    }
+    bytes[] memory calldatas = new bytes[](1);
+    {
+      calldatas[0] = abi.encodeWithSelector(TimelockController.updateDelay.selector, _newDelay);
+    }
+
+    timelockController.schedule(targets[0], values[0], calldatas[0], bytes32(0), bytes32(0), 0);
+    timelockController.execute(targets[0], values[0], calldatas[0], bytes32(0), bytes32(0));
+
+    uint256 newMinDelay = timelockController.getMinDelay();
+    console2.log(newMinDelay);
+    assert(newMinDelay == _newDelay);
   }
 }
