20c59a43ccbe05296982d06458aa85dbfee16bbe
MrDeadCe11 <mister.deadce11@gmail.com>
2024-05-02 17:25:40 -0500

    added more tests

--- test/e2e/E2ESafeManager.t.sol
+++ test/e2e/E2ESafeManager.t.sol
@@ -7,6 +7,7 @@ import {BaseUser} from '@test/scopes/BaseUser.t.sol';
 import {DirectUser} from '@test/scopes/DirectUser.t.sol';
 import {ProxyUser} from '@test/scopes/ProxyUser.t.sol';
 import {IODSafeManager} from '@interfaces/proxies/IODSafeManager.sol';
+import {SafeSaviourForForTest} from '@test/mocks/SafeSaviourForTest.sol';
 
 import {HOUR, YEAR} from '@libraries/Math.sol';
 
@@ -14,14 +15,24 @@ abstract contract E2ESafeMangerSetUp is Base_CType, Common {
   address aliceProxy;
   address bobProxy;
 
+  address testSaviour;
+
   uint256 aliceSafeId;
   uint256 bobSafeId;
+
+  IODSafeManager.SAFEData public aliceData;
+
+  event TransferSAFEOwnership(address indexed _sender, uint256 indexed _safe, address _dst);
 
   function setUp() public override {
     super.setUp();
     super.setupEnvironment();
     aliceProxy = deployOrFind(alice);
     aliceSafeId = safeManager.openSAFE(_cType(), aliceProxy);
+    aliceData = safeManager.safeData(aliceSafeId);
+    testSaviour = address(new SafeSaviourForForTest());
+    vm.prank(address(timelockController));
+    liquidationEngine.connectSAFESaviour(testSaviour);
   }
 
   function deployOrFind(address owner) public returns (address payable) {
@@ -35,6 +46,13 @@ abstract contract E2ESafeMangerSetUp is Base_CType, Common {
 
   function _cType() internal pure override returns (bytes32) {
     return bytes32(abi.encodePacked('CTYPE1'));
+  }
+
+  function _removeDelays() internal {
+    vm.startPrank(vault721.timelockController());
+    vault721.modifyParameters('timeDelay', abi.encode(0 days));
+    vault721.modifyParameters('blockDelay', abi.encode(0));
+    vm.stopPrank();
   }
 }
 
@@ -58,13 +76,12 @@ contract E2ESafeManagerTest is E2ESafeMangerSetUp {
   }
 
   function test_GetSafeDataFromHandler() public view {
-    (, address[] memory safeHandlers,) = safeManager.getSafesData(aliceProxy);
-    IODSafeManager.SAFEData memory _data = safeManager.getSafeDataFromHandler(safeHandlers[0]);
+    IODSafeManager.SAFEData memory _data = safeManager.getSafeDataFromHandler(aliceData.safeHandler);
 
     assertEq(_data.nonce, 0);
     assertEq(_data.owner, aliceProxy);
     assertEq(_data.collateralType, _cType());
-    assertEq(_data.safeHandler, safeHandlers[0]);
+    assertEq(_data.safeHandler, aliceData.safeHandler);
   }
 
   function test_SafeData() public view {
@@ -72,7 +89,75 @@ contract E2ESafeManagerTest is E2ESafeMangerSetUp {
     assertEq(_data.nonce, 0);
     assertEq(_data.owner, aliceProxy);
     assertEq(_data.collateralType, _cType());
+    assertEq(_data.safeHandler, aliceData.safeHandler);
   }
 
-  function test_AllowSafe() public {}
+  function test_AllowSafe() public {
+    vm.prank(alice);
+    assertFalse(safeManager.safeCan(aliceProxy, aliceSafeId, 0, alice));
+    vm.prank(aliceProxy);
+    safeManager.allowSAFE(aliceSafeId, alice, true);
+
+    vm.prank(alice);
+    assertTrue(safeManager.safeCan(aliceProxy, aliceSafeId, 0, alice));
+  }
+
+  function test_OpenSafe() public {
+    bobProxy = deployOrFind(bob);
+    vm.prank(bobProxy);
+    bobSafeId = safeManager.openSAFE(_cType(), bobProxy);
+    assertEq(safeManager.getSafes(bobProxy).length, 1);
+    assertEq(vault721.ownerOf(bobSafeId), bob);
+  }
+
+  function test_TransferSafeOwnership() public {
+    bobProxy = deployOrFind(bob);
+    //set safe savior to test savior clearing on transfer
+    vm.prank(aliceProxy);
+    safeManager.protectSAFE(aliceSafeId, testSaviour);
+    assertEq(liquidationEngine.chosenSAFESaviour(_cType(), aliceData.safeHandler), testSaviour);
+
+    _removeDelays();
+    vm.startPrank(alice);
+    vm.expectEmit(address(safeManager));
+    emit TransferSAFEOwnership(address(vault721), aliceSafeId, bobProxy);
+    vault721.transferFrom(alice, bob, aliceSafeId);
+    vm.stopPrank();
+
+    aliceData = safeManager.safeData(aliceSafeId);
+
+    assertEq(liquidationEngine.chosenSAFESaviour(_cType(), aliceData.safeHandler), address(0));
+    assertEq(vault721.ownerOf(aliceSafeId), bob);
+    assertEq(aliceData.nonce, 1);
+    assertEq(safeManager.getSafes(aliceProxy).length, 0);
+    assertEq(safeManager.getSafes(bobProxy).length, 1);
+    assertEq(aliceData.owner, bobProxy);
+  }
+
+  function test_TransferSafeOwnership_Revert_ZeroAddress() public {
+    _removeDelays();
+
+    vm.expectRevert(IODSafeManager.ZeroAddress.selector);
+    vm.startPrank(address(vault721));
+    safeManager.transferSAFEOwnership(aliceSafeId, address(0));
+    vm.stopPrank();
+  }
+
+  function test_TransferSafeOwnership_Revert_Vault721Only() public {
+    _removeDelays();
+
+    vm.expectRevert('SafeMngr: Only Vault721');
+    vm.startPrank(alice);
+    safeManager.transferSAFEOwnership(aliceSafeId, address(0));
+    vm.stopPrank();
+  }
+
+  function test_TransferSafeOwnership_Revert_AlreadySafeOwner() public {
+    _removeDelays();
+
+    vm.expectRevert(IODSafeManager.AlreadySafeOwner.selector);
+    vm.startPrank(alice);
+    vault721.transferFrom(alice, alice, aliceSafeId);
+    vm.stopPrank();
+  }
 }
