42948f1d0cf2718a63b9a32053c4a44e661a9319
Hunter King <hking6776@gmail.com>
2024-05-02 21:01:04 -0700

    create PrankSwitch contract

--- script/postdeployment/DeployStaticOracle.s.sol
+++ script/postdeployment/DeployStaticOracle.s.sol
@@ -2,8 +2,7 @@
 pragma solidity 0.8.20;
 
 import '@script/Registry.s.sol';
-import {Script} from 'forge-std/Script.sol';
-import {Test} from 'forge-std/Test.sol';
+import {PrankSwitch} from '@script/utils/PrankSwitch.s.sol';
 import {MainnetDeployment} from '@script/MainnetDeployment.s.sol';
 import {HardcodedOracle} from '@contracts/for-test/HardcodedOracle.sol';
 
@@ -13,29 +12,10 @@ import {HardcodedOracle} from '@contracts/for-test/HardcodedOracle.sol';
 // SIMULATE
 // source .env && forge script DeployStaticOracle --with-gas-price 2000000000 -vvvvv --rpc-url $ARB_MAINNET_RPC
 
-contract DeployStaticOracle is MainnetDeployment, Script, Test {
-  address internal constant _DEPLOYER = 0xF78dA2A37049627636546E0cFAaB2aD664950917;
-  uint256 internal _deployerPk;
-  address internal _deployer;
-
+contract DeployStaticOracle is MainnetDeployment, PrankSwitch {
   address internal _initialSystemCoinOracle;
 
-  modifier prankSwitch(address _caller, address _account) {
-    bool _broadcast;
-    if (_caller == _account) _broadcast = true;
-    if (_broadcast) vm.startBroadcast(_deployerPk);
-    else vm.startPrank(_account);
-    _;
-    if (_broadcast) vm.stopBroadcast();
-    else vm.stopPrank();
-  }
-
-  function setUp() public {
-    _deployerPk = vm.envUint('ARB_MAINNET_DEPLOYER_PK');
-    _deployer = vm.addr(_deployerPk);
-  }
-
-  function run() public prankSwitch(_deployer, _DEPLOYER) {
+  function run() public prankSwitch(_deployer, MAINNET_TIMELOCK_CONTROLLER) {
     _initialSystemCoinOracle = address(new HardcodedOracle('OD / USD', 1e18));
     emit log_named_address('SystemCoinOracle Address', _initialSystemCoinOracle);
 
